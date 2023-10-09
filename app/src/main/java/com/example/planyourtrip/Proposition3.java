957df900a484ebbd44bb7deba83b7f7924c7976c
SednaRaffaud <sedna.raffaud@orange.fr>
2023-10-09 15:18:33 +0200

    commit

---
+++ app/src/main/java/com/example/planyourtrip/Proposition3.java
@@ -0,0 +1,14 @@
+package com.example.planyourtrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Proposition3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proposition3);
+    }
+}
